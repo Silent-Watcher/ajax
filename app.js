a85579b663c46a70a13b129a7a6fdabb73a74938
Ali.t.nazari <91375198+Silent-Watcher@users.noreply.github.com>
2021-10-17 21:50:54 +0330

    fix some bugs

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
   $("input").keyup(function () {
-      var value = $(this).val();
-      if (value == "") {
-          $(".result-list").html("");
-      } else {
-          $.ajax({
-            type: $(".searchWp").attr("method"),
-            url: $(".searchWp").attr("action"),
-            data: $(".searchWp").serialize(),
-           success: function (response) {     
-                     $(".result-list").html(response);
-            },
-          });
-      }
+    var value = $(this).val();
+    if (value == "") {
+      $(".result-list").html("");
+      $(".searchInput").removeClass(" activeSearch"); //
+    } else {
+      $(".searchInput").addClass(" activeSearch"); //
+      $.ajax({
+        type: $(".searchWp").attr("method"),
+        url: $(".searchWp").attr("action"),
+        data: $(".searchWp").serialize(),
+        success: function (response) {
+          $(".result-list").html(response);
+        },
+        error: function (response) {
+          console.log(response);
+        },
+      });
+    }
   });
 });
